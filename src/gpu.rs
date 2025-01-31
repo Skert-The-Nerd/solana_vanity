dab2ab94328765e5ad72723f947d71f73b69b832
Skert-The-Nerd <skert6969@proton.me>
2025-01-31 00:23:25 -0600

    Update gpu.rs

--- src/gpu.rs
+++ src/gpu.rs
@@ -9,14 +9,20 @@ pub fn grind(target: String, case_insensitive: bool, num_threads: u32) {
     let kernel_source = include_str!("gpu_kernel.cl");
 
     let program = Program::builder().src(kernel_source).build(&context).unwrap();
-    let buffer = Buffer::<u8>::builder().queue(queue.clone()).len(num_threads).build().unwrap();
+    let seeds = Buffer::<u8>::builder().queue(queue.clone()).len(32 * num_threads).build().unwrap();
+    let results = Buffer::<u8>::builder().queue(queue.clone()).len(32).build().unwrap();
+    let match_found = Buffer::<i32>::builder().queue(queue.clone()).len(1).build().unwrap();
 
     let kernel = Kernel::builder()
         .program(&program)
         .name("vanity_search")
         .queue(queue.clone())
         .global_work_size(num_threads)
-        .arg(&buffer)
+        .arg(&seeds)
+        .arg(&results)
+        .arg(&match_found)
+        .arg(&target)
+        .arg(target.len() as i32)
         .build().unwrap();
 
     println!("Launching GPU kernel...");
@@ -24,5 +30,5 @@ pub fn grind(target: String, case_insensitive: bool, num_threads: u32) {
         kernel.enq().unwrap();
     }
 
-    println!("GPU acceleration is running!");
+    println!("GPU processing complete!");
 }
