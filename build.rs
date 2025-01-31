9f8f505bf9d4db95f74b1385c76c4266cbd4b747
Skert-The-Nerd <skert6969@proton.me>
2025-01-31 13:58:21 -0600

    Update build.rs

--- build.rs
+++ build.rs
@@ -5,7 +5,7 @@ fn main() {
     if cfg!(feature = "gpu") {
         // Link against OpenCL library
         println!("cargo:rustc-link-lib=OpenCL");
-
+        
         // If you have custom OpenCL or CUDA code to compile, use the 'cc' crate
         // Example: Compiling an OpenCL kernel written in C
         // Uncomment and modify the following lines if you have C/C++ GPU kernels
