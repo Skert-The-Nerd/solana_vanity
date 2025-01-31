d2e8efd3bfe3f5d835e38d6521cbd370dfbaf6cf
Skert-The-Nerd <skert6969@proton.me>
2025-01-31 11:07:00 -0600

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,17 +1,5 @@
-// build.rs
-
 fn main() {
-    // If the 'gpu' feature is enabled, perform GPU-related build steps
     if cfg!(feature = "gpu") {
-        // Link against OpenCL library
         println!("cargo:rustc-link-lib=OpenCL");
-
-        // If you have custom OpenCL or CUDA code to compile, use the 'cc' crate
-        /*
-        cc::Build::new()
-            .file("src/gpu_kernel.c") // Path to your GPU kernel C file
-            .include("src/") // Include path if necessary
-            .compile("gpu_kernel");
-        */
     }
 }
